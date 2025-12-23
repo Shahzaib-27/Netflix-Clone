360c44d95af56ee099f3723093a1e09ab86617de
Shahzaib <shazuthebigbrother@gmail.com>
2025-12-23 17:38:10 +0500

    change simthing

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import LayoutData from "./Components/Layout/Layout.jsx";
 
-
-
 export default function App() {
   return (
     <>
 
-
     <BrowserRouter>
       <Routes>
-
 
         <Route path="/" element={<LayoutData/>} />
 
