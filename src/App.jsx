8c8b03fe4b49a255dba41c622bed855c89249e05
Shahzaib Shahid <shazuthebigbrother@gmail.com>
2026-01-05 20:09:36 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HedearData from "./header.jsx"
+import MainSecData from "./main_section.jsx"
+import FooterAPP from "./Footer.jsx"
+
+function App() {
+ 
+
+  return (
+    <>
+    <HedearData/>
+    <MainSecData/>
+    <FooterAPP/>
+    </>
+  );
+}
+
+export default App
